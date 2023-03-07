8b1e428a62faaff27fe89f8277720821c2800c4a
Ethancolt <97382593+Ethancolt@users.noreply.github.com>
2023-03-07 15:40:02 +1300

    Initial Setup

---
+++ Solitaire/src/solitaire/Solitaire.java
@@ -0,0 +1,16 @@
+package solitaire;
+
+/**
+ *
+ * @author Brand
+ */
+public class Solitaire {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
