3824343df2973d67b8c073b571f02e734da8fcf5
Ethancolt <97382593+Ethancolt@users.noreply.github.com>
2023-04-04 17:24:32 +1200

    Update Play part 2

--- Solitaire/src/solitaire/Play.java
+++ Solitaire/src/solitaire/Play.java
@@ -15,6 +15,22 @@ public class Play {
     Waste waste = new Waste();
     Table[] table = new Table[7];
     ArrayList<Card> newDeck = new ArrayList<>();
+    
+    public Play() {
+        
+        for (int i = 0; i < 4; i++) {
+            
+            foundation[i] = new Foundation();
+            
+        }
+        
+        for (int i = 0; i < 7; i++) {
+            
+            table[i] = new Table();
+            
+        }
+        
+    }
 
     public static void main(String[] args) {
         
@@ -45,7 +61,7 @@ public class Play {
         //Lay out cards for the Table
         for (int column = 0; column < 7; column++) {
             
-            for (int row = 0; row < column; row++) {
+            for (int row = 0; row < column + 1; row++) {
                 
                 table[column].addCard(newDeck.remove(0));
                 
@@ -54,7 +70,9 @@ public class Play {
         }
         
         //Put the rest of the cards into the Stock
-        for (Card card : newDeck) {
+        int size = newDeck.size();
+        
+        for (int i = 0; i < size; i++) {
             
             stock.addCard(newDeck.remove(0));
             
@@ -66,7 +84,7 @@ public class Play {
         
         //todo: display cards
         
-        
+        System.out.println("Table: "+table+"\nStock: "+stock+"\nWaste: "+waste+"\nFoundation: "+foundation);
         
         //ask user what action they would like to take
         Scanner keyboard = new Scanner(System.in);
@@ -81,39 +99,158 @@ public class Play {
             //check if the card is valid
             if (isValidCard(command[0])) {
                 
-                //check if the card is on top, and return it
-                Card currentCard = findCard(command[0]);
+                //check if the card is on top, and return the pile
+                CardPile currentPile = findPile(command[0]);
                 
-                if (currentCard != null) {
+                if (currentPile.getTopCard() != null) {
                     
-                    //todo: perform commands
+                    //checks which command the user is performing, and executes that command.
                     switch (command[1]) {
                         case "t1":
-
+                            
+                            table[0].addCard(currentPile.removeCard());
+                            
                             break;
                         case "t2":
 
+                            table[1].addCard(currentPile.removeCard());
+                            
                             break;
                         case "t3":
 
+                            table[2].addCard(currentPile.removeCard());
+                            
                             break;
                         case "t4":
 
+                            table[3].addCard(currentPile.removeCard());
+                            
                             break;
                         case "t5":
 
+                            table[4].addCard(currentPile.removeCard());
+                            
                             break;
                         case "t6":
 
+                            table[5].addCard(currentPile.removeCard());
+                            
                             break;
                         case "t7":
 
+                            table[6].addCard(currentPile.removeCard());
+                            
                             break;
                         case "f":
 
+                            //todo: check if card can be placed in foundation.
+                            
+                            switch (currentPile.getTopCard().suit.icon) {
+                                case '♦':
+                                    
+                                    if (foundation[0].getTopCard() != null) {
+                                        
+                                        // if there are cards in the foundation pile, check if the card being placed is 1 higher.
+                                        if (foundation[0].getTopCard().value.ordinal() + 1 == currentPile.getTopCard().value.ordinal()) {
+                                            
+                                            foundation[0].addCard(currentPile.removeCard());
+                                            
+                                        }
+                                        
+                                    } else {
+                                        
+                                        // if the foundation pile is empty, check if the card is an ace.
+                                        if (currentPile.getTopCard().value == FaceValue.ACE) {
+                                            
+                                            foundation[0].addCard(currentPile.removeCard());
+                                            
+                                        }
+                                        
+                                    }
+                                    
+                                    break;
+                                case '♥':
+                                    
+                                    if (foundation[1].getTopCard() != null) {
+                                        
+                                        // if there are cards in the foundation pile, check if the card being placed is 1 higher.
+                                        if (foundation[1].getTopCard().value.ordinal() + 1 == currentPile.getTopCard().value.ordinal()) {
+                                            
+                                            foundation[1].addCard(currentPile.removeCard());
+                                            
+                                        }
+                                        
+                                    } else {
+                                        
+                                        // if the foundation pile is empty, check if the card is an ace.
+                                        if (currentPile.getTopCard().value == FaceValue.ACE) {
+                                            
+                                            foundation[1].addCard(currentPile.removeCard());
+                                            
+                                        }
+                                        
+                                    }
+                                    
+                                    break;
+                                case '♠':
+                                    
+                                    if (foundation[2].getTopCard() != null) {
+                                        
+                                        // if there are cards in the foundation pile, check if the card being placed is 1 higher.
+                                        if (foundation[2].getTopCard().value.ordinal() + 1 == currentPile.getTopCard().value.ordinal()) {
+                                            
+                                            foundation[2].addCard(currentPile.removeCard());
+                                            
+                                        }
+                                        
+                                    } else {
+                                        
+                                        // if the foundation pile is empty, check if the card is an ace.
+                                        if (currentPile.getTopCard().value == FaceValue.ACE) {
+                                            
+                                            foundation[2].addCard(currentPile.removeCard());
+                                            
+                                        }
+                                        
+                                    }
+                                    
+                                    break;
+                                case '♣':
+                                    
+                                    if (foundation[3].getTopCard() != null) {
+                                        
+                                        // if there are cards in the foundation pile, check if the card being placed is 1 higher.
+                                        if (foundation[3].getTopCard().value.ordinal() + 1 == currentPile.getTopCard().value.ordinal()) {
+                                            
+                                            foundation[3].addCard(currentPile.removeCard());
+                                            
+                                        }
+                                        
+                                    } else {
+                                        
+                                        // if the foundation pile is empty, check if the card is an ace.
+                                        if (currentPile.getTopCard().value == FaceValue.ACE) {
+                                            
+                                            foundation[3].addCard(currentPile.removeCard());
+                                            
+                                        }
+                                        
+                                    }
+                                    
+                                    break;
+                                default:
+                                    
+                                    System.out.println("Error, please try again!");
+                                    
+                                    break;
+                            }
+                            
                             break;
 
                         default:
+                            
+                            System.out.println("Card pile not found.");
+                            
                             break;
 
                     }
@@ -167,11 +304,59 @@ public class Play {
         
     }
     
-    public Card findCard(String card) {
+    /**
+     * Returns the CardPile if the card is found on top of that pile.
+     **/
+    public CardPile findPile(String card) {
         
         char[] cardValue = card.toCharArray();
         
-        //todo: check if card is on top, and return the card
+        switch (cardValue[1]) {
+            case 'd':
+                
+                cardValue[1] = '♦';
+                
+                break;
+            case 'h':
+                
+                cardValue[1] = '♥';
+                
+                break;
+            case 's':
+                
+                cardValue[1] = '♠';
+                
+                break;
+            case 'c':
+                
+                cardValue[1] = '♣';
+                
+                break;
+            default:
+                throw new AssertionError();
+        }
+        
+        Card top = null;
+        
+        for (Table t : table) {
+            
+            top = t.getTopCard();
+            
+            if (top.value.icon == cardValue[0] && top.suit.icon == cardValue[1]) {
+                
+                return t;
+                
+            }
+            
+        }
+        
+        top = waste.getTopCard();
+        
+        if (top.value.icon == cardValue[0] && top.suit.icon == cardValue[1]) {
+                
+                return waste;
+                
+        }
         
         return null;
         
