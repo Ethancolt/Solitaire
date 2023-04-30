e6672352cb39806c20da396d0b886766ba5c97fd
Itstr <Itstr@172.30.44.192>
2023-04-30 21:27:13 +1200

    extra space

--- Solitaire/src/solitaire/DisplayCards.java
+++ Solitaire/src/solitaire/DisplayCards.java
@@ -11,20 +11,61 @@ package solitaire;
 public class DisplayCards 
 {
 
-    public static void displayTables (Table t1, Table t2, Table t3, Table t4, Table t5,Table t6, Table t7) //Displays the Table piles in the game
+public static void displayTables(Table t1, Table t2, Table t3, Table t4, Table t5, Table t6, Table t7) 
+{
+    int maxSize = getMaxSize(t1, t2, t3, t4, t5, t6, t7);
+    
+    System.out.printf("%-20s%-20s%-20s%-20s%-20s%-20s%-20s%n", 
+            "Table 1", "Table 2", "Table 3",
+            "Table 4", "Table 5", "Table 6",
+            "Table 7");
+    
+    for (int i = maxSize - 1; i >= 0; i--) 
     {
-            System.out.println("Table0:\n" + t1
-            + "\nTable1:\n" + t2
-            + "\nTable2:\n" + t3
-            + "\nTable3:\n" + t4
-            + "\nTable4:\n" + t5
-            + "\nTable5:\n" + t6
-            + "\nTable6:\n" + t7);
+        System.out.printf("%-20s%-20s%-20s%-20s%-20s%-20s%-20s%n", 
+                getCard(t1, i), 
+                getCard(t2, i), 
+                getCard(t3, i),
+                getCard(t4, i),
+                getCard(t5, i), 
+                getCard(t6, i), 
+                getCard(t7, i));
+    }
+}
+
+    private static int getMaxSize(Table... tables) 
+    {
+        int maxSize = 0;
+        for (Table table : tables) 
+        {
+            int size = table.getSize();
+            if (size > maxSize) 
+            {
+                maxSize = size;
+            }
+        }
+        return maxSize;
+    }
+
+    private static String getCard(Table table, int index) 
+    {
+        if (index < table.getSize()) 
+        {
+            if(table.getCardByIndex(index).faceUp == true)
+            {
+                return table.getCardByIndex(index).toString();
+            }
+            return "[??]";
+        } 
+        else 
+        {
+            return "";
+        }
     }
     
     public static void displayStock(Stock stock) //Displays the Stock pile in the Game
     {
-        System.out.println("\nStock:\n" + stock);
+        System.out.println("Stock:  " + "[" + stock.cards.size() + "]"); 
     }
     
     public static void displayWaste(Waste waste) // Displays the Waste pile in the game
@@ -39,6 +80,7 @@ public class DisplayCards
             + "\nFoundation2:\n" + f3
             + "\nFoundation3:\n" + f4);
     }
+    
 
     
 }
