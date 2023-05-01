d12962e5f88ff71c1f05b5fb4edcc5011465f3d7
Itstr <Itstr@172.30.44.192>
2023-05-01 15:52:31 +1200

    Added SaveTime

---
+++ Solitaire/src/solitaire/SaveTime.java
@@ -0,0 +1,131 @@
+
+package solitaire;
+
+/**
+ *
+ * @author Trey Baker [21155292]
+ */
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class SaveTime 
+{
+
+    private static final String FILENAME = "times.txt";
+    private static final int MAX_TIMES = 10;
+
+    public static void saveTime(String playerName, long elapsedTime)  
+    {
+
+        ArrayList<TimeRecord> times = loadTimes();
+
+        if (times.size() >= MAX_TIMES && elapsedTime >= times.get(times.size() - 1).elapsedTime) 
+        {
+            System.out.println("\nYour time was not fast enough to make the leaderboard!");
+            return;
+        }
+
+        times.add(new TimeRecord(playerName, elapsedTime));
+        Collections.sort(times);
+
+        if (times.size() > MAX_TIMES) 
+        {
+            times.subList(MAX_TIMES, times.size()).clear();
+        }
+
+        try 
+        {
+            FileWriter writer = new FileWriter(FILENAME);
+            for (TimeRecord time : times) 
+            {
+                writer.write(time.playerName + "," + time.elapsedTime + "\n");
+            }
+            writer.close();
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static void displayTimes() 
+    {
+
+        ArrayList<TimeRecord> times = loadTimes();
+
+        System.out.println("Top 10 times:");
+
+        int count = 0;
+
+        for (int i = 0; i < times.size(); i++) 
+        {
+
+            if (count >= MAX_TIMES) 
+            {
+                break;
+            }
+
+            TimeRecord time = times.get(i);
+            System.out.println(time.playerName + ": " + time.elapsedTime + " seconds");
+            count++;
+        }
+    }
+
+    private static ArrayList<TimeRecord> loadTimes() 
+    {
+
+        ArrayList<TimeRecord> times = new ArrayList<>();
+
+        try 
+        {
+            File file = new File(FILENAME);
+            if (!file.exists()) 
+            {
+                return times;
+            }
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) 
+            {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+                
+                if (parts.length == 2) 
+                {
+                    String playerName = parts[0];
+                    long elapsedTime = Long.parseLong(parts[1]);
+                    times.add(new TimeRecord(playerName, elapsedTime));
+                }
+            }
+            scanner.close();
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+
+        return times;
+    }
+
+    private static class TimeRecord implements Comparable<TimeRecord> 
+    {
+        public String playerName;
+        public long elapsedTime;
+
+        public TimeRecord(String playerName, long elapsedTime) 
+        {
+            this.playerName = playerName;
+            this.elapsedTime = elapsedTime;
+        }
+
+        @Override
+        public int compareTo(TimeRecord other) 
+        {
+            return Long.compare(this.elapsedTime, other.elapsedTime);
+        }
+    }
+
+}
