8b4ad47c82be67e8ade631ec91062d869b17f654
Ethancolt <97382593+Ethancolt@users.noreply.github.com>
2023-06-09 21:58:47 +1200

    menu fix

--- Solitaire/src/SolitaireGUI/Play.java
+++ Solitaire/src/SolitaireGUI/Play.java
@@ -1,42 +1,48 @@
-
 package SolitaireGUI;
 
-
 import java.awt.Color;
+import javax.swing.SwingWorker;
 
 /**
  *
  * @author Trey Baker [21155292]
  */
-public class Play 
-{
+public class Play {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-  
-        
-        
+
         GameOpen openMenu = new GameOpen();
-        
-        if (openMenu.isGameReady())
-        {
-        MainForm solitaire;
-        
-        GamePanel game = new GamePanel();
-        game.setSize(ConstantValues.gameWidth, ConstantValues.gameHeight);
-        game.setBackground(Color.yellow);
-        
-        // Add components and properties to main form
-        solitaire = new MainForm();
-        solitaire.pack();
-        solitaire.setVisible(true);
-        solitaire.setResizable(false);
-        solitaire.setSize(ConstantValues.gameWidth, ConstantValues.gameHeight);
-        solitaire.add(game);
-        }
-        
+
+        SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+            @Override
+            protected Void doInBackground() throws Exception {
+                while (!openMenu.isGameReady()) {
+                    Thread.sleep(100); // Wait until the game is ready
+                }
+                return null;
+            }
+
+            @Override
+            protected void done() {
+                // Create and show the game panel
+                GamePanel game = new GamePanel();
+                game.setSize(ConstantValues.gameWidth, ConstantValues.gameHeight);
+                game.setBackground(Color.yellow);
+
+                MainForm solitaire = new MainForm();
+                solitaire.pack();
+                solitaire.setResizable(false);
+                solitaire.setSize(ConstantValues.gameWidth, ConstantValues.gameHeight);
+                solitaire.add(game);
+                solitaire.setVisible(true);
+            }
+        };
+
+        worker.execute();
+
     }
-    
+
 }
